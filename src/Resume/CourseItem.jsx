a4c69e029b31def372716c10f4dcfec44214fb36
Vika <svirin.victoria@gmail.com>
2023-07-21 18:54:51 +0200

    Changes of Css, icons, credits usw.

--- src/Resume/CourseItem.jsx
+++ src/Resume/CourseItem.jsx
@@ -1,10 +1,10 @@
-import right_arrow from '../images/right_arrow.png'; 
+import right_arrow from '../images/common/arrow.png'; 
 function CourseItem(props){
 
     return(
         <div className='exp-text'>
-              <img className='arrow_icon' src={right_arrow} alt="arrow" />
-              <span >{props.courseName}</span>
+            <img className='arrow_icon' src={right_arrow} alt="arrow" />
+            <span >{props.courseName}</span>
         </div>
     )
 }
--- src/Resume/Languages.jsx
+++ src/Resume/Languages.jsx
@@ -1,6 +1,20 @@
 
+import starfull from "../images/common/star-full.png";
+import starempty from "../images/common/star-empty.png";
 
 function Languages(props){
+
+  const getRaiting = () => {
+    const stars = [];
+    for (let i = 0; i < 5; i++) {
+      stars.push(<img key={i} className='rating_star' 
+      src={
+        (i < props.rating) ? starfull : starempty
+      } alt="rating" />); 
+
+    }
+    return stars;
+  }
 
   return(
     <div className='rating_home'>  
@@ -8,8 +22,8 @@ function Languages(props){
         <img className="flag_location" src={props.flag} alt="Flag"/>
         <span className='after_icon'>{props.lang}</span>
       </div>
-      <div> 
-        <img className='rating_star' src={props.rating} alt="rating" />
+      <div className="ratingBox"> 
+        {getRaiting()}
       </div>
     </div>
 
