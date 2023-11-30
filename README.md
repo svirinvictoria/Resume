28f64498a5f2db596b893ace4daf97c2f5bd3af1
Svirin Victoria <svirin.victoria@gmail.com>
2023-11-30 16:42:40 +0100

    Fix responsivity, add icons, add techs

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Resume
 CV page
+
+Wellcome to my Portfolio front page!
+Links to my other projects can be found in section "Portfolio" in upper right corner. All the projects are online and can be reached by clicking on the link.
+
+###
+This project is written using React.js and CSS for styling. The Website is belingual and language can be changed by clicking on the flag button in the upper right corner. The relevant information is located in the demo-server (useProxy hook).
+The icons are taken from "https://uxwing.com//"
+
+###
+###
+This is a list of React features used in this project:
+###
+*hooks
+*Routing & Navigation
+*useEffect, useState, useLocation, useNavigate
+*mapping
+
+### 
+This is a list os CSS features:
+*flex-box
+*.png icons
+
--- src/Resume/Resume.jsx
+++ src/Resume/Resume.jsx
@@ -1,21 +1,36 @@
-import './Resume.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import "./Resume.css";
 import { useEffect, useState } from "react";
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from "react-router-dom";
 import { useProxy } from "../hooks/useProxy";
-import foto from "../images/foto.jpg"
-import Topic from './Topic';
+import foto from "../images/foto.jpg";
+import Topic from "./Topic";
 import CourseItem from "./CourseItem";
 import PersonalItem from "./Languages";
+
 import flag_ger from "../images/lang/gerCircle.png";
 import flag_eng from "../images/lang/ukCircle.png";
-import flag_isr from "../images/lang/isrCircle.png"
+import flag_isr from "../images/lang/isrCircle.png";
 import flag_rus from "../images/lang/rusCircle.png";
 
-import linkedin from "../images/common/linkedin.png"
+import icoCommon from "../images/technology/tech.png";
+import icoJavascript from "../images/technology/javascript.png";
+import icoReactJs from "../images/technology/reactjs.png";
+import icoMongo from "../images/technology/mongodb-icon.png";
+import icoCss from "../images/technology/css-icon.png";
+import icoHtml from "../images/technology/html-icon.png";
+import icoBootstrap from "../images/technology/bootstrap-5-logo-icon.png";
+import icoSass from "../images/technology/sass-icon.png";
+import icoNode from "../images/technology/node-js-icon.png";
+
+import linkedin from "../images/common/linkedin.png";
 import folder from "../images/common/briefcase-icon.png";
+import eLearning from "../images/common/e-learning-icon.png";
+import graduation from "../images/common/graduation-cap-icon.png";
+import manager from "../images/common/manager-icon.png";
+import personalInfo from "../images/common/personal-info-icon.png";
 
 function Resume() {
-
   const [info, setInfo] = useState({});
   const location = useLocation();
   const proxy = useProxy();
@@ -23,61 +38,122 @@ function Resume() {
 
   useEffect(() => {
     const personalInfo = proxy.renderPage(location.pathname);
-    setInfo(personalInfo)
+    setInfo(personalInfo);
   }, [location.pathname]);
 
   function goToEnglishHandler() {
-    navigate("/en")
+    navigate("/en");
   }
 
   function goToGermanHandler() {
-    navigate("/de")
+    navigate("/de");
+  }
+
+  function getTechnologyIcon(icon) {
+    switch (icon) {
+      case "javascript":
+        return icoJavascript;
+      case "reactjs":
+        return icoReactJs;
+      case "html":
+        return icoHtml;
+      case "css":
+        return icoCss;
+      case "mongoDb":
+        return icoMongo;
+      case "scss":
+        return icoSass;
+      case "bootstrap":
+        return icoBootstrap;
+      case "node":
+        return icoNode;
+      default:
+        return icoCommon;
+    }
   }
 
   return (
-    <div className='container'>
-
+    <div className="container">
       <div className="personal_data">
-
         <p className="name">{info.name}</p>
 
-        <div className='change_view'>
-          <div className='photo_frame'>
-            <img className='photo_img' alt="" src={foto} />
+        <div className="change_view">
+          <div className="photo_frame">
+            <img className="photo_img" alt="" src={foto} />
           </div>
           <div className="info change_personal">
-            <p className="heading change_personal_title">{info.personalTitel}</p>
-            <p className='personalSection'>
-              <span><img className="imgLink square" src={linkedin} alt='' /></span>
-              <span><a target='_blank' rel="noreferrer" className='personalLink' href={info.linkedin}>{info.linkedinTitle}</a></span>
+            <p className="heading change_personal_title">
+              {info.personalTitel}
+            </p>
+            <p className="personalSection">
+              <span>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  className="personalLink"
+                  href={info.linkedin}
+                >
+                  <span>
+                    <img className="imgLink square" src={linkedin} alt="" />
+                  </span>
+                  {info.linkedinTitle}
+                </a>
+              </span>
             </p>
           </div>
 
-          <hr className='line_left'></hr>
+          <hr className="line_left"></hr>
           <div className="info change_personal">
-            <p className="heading change_personal_title">{info.portfolioTitle}</p>
-              {info && info.portfolio && info.portfolio.map(item => {
+            <p className="heading change_personal_title">
+              {info.portfolioTitle}
+            </p>
+            {info &&
+              info.portfolio &&
+              info.portfolio.map((item) => {
                 return (
-                  <p className='personalSection'>
+                  <p className="personalSection">
                     <div>
-                      <span><img className="imgLink" src={folder} alt='' /></span>
-                      <span><a target='_blank' rel="noreferrer" className='personalLink' href={item.link}>{item.title}</a></span>
+                      <span>
+                        <img className="imgLink" src={folder} alt="" />
+                      </span>
+                      <span>
+                        <a
+                          target="_blank"
+                          rel="noreferrer"
+                          className="personalLink"
+                          href={item.link}
+                        >
+                          {item.title}
+                        </a>
+                      </span>
                     </div>
                   </p>
                 );
               })}
-           </div>
+          </div>
         </div>
 
-        <hr className='line_left'></hr>
+        <hr className="line_left"></hr>
         <div className="info">
           <p className="heading">{info.progLanguages}</p>
-          <div>{info.html}</div>
-          <div>{info.css}</div>
-          <div>{info.js} </div>
-          <div>{info.react}</div>
+          {info &&
+            info.technology &&
+            info.technology.map((tech) => {
+              return (
+                <div className="technology">
+                  <div>
+                    <img
+                      className="techIcon"
+                      src={getTechnologyIcon(tech.icon)}
+                      alt={tech.name}
+                    />
+                  </div>
+                  <div>{tech.name}</div>
+                </div>
+              );
+            })}
         </div>
-        <hr className='line_left'></hr>
+        <hr className="line_left"></hr>
         <div className="info">
           <p className="heading">{info.personalSkills}</p>
           <div>{info.skillsCommunication}</div>
@@ -85,41 +161,67 @@ function Resume() {
           <div>{info.skillsResponsibility}</div>
           <div>{info.skillsPurpose}</div>
         </div>
-        <hr className='line_left'></hr>
+        <hr className="line_left"></hr>
 
         <div className="info">
           <p className="heading">{info.languages}</p>
           <PersonalItem flag={flag_ger} lang={info.langGerm} rating={4} />
-          <PersonalItem flag={flag_eng} lang={info.langEng}  rating={4} />
-          <PersonalItem flag={flag_isr} lang={info.langHeb}  rating={5} />
-          <PersonalItem flag={flag_rus} lang={info.langRus}  rating={5} />
+          <PersonalItem flag={flag_eng} lang={info.langEng} rating={4} />
+          <PersonalItem flag={flag_isr} lang={info.langHeb} rating={5} />
+          <PersonalItem flag={flag_rus} lang={info.langRus} rating={5} />
         </div>
-        <hr className='line_left'></hr>
+        <hr className="line_left"></hr>
       </div>
-
-      <div className='description'>
-        <button className='btn btn-ger' onClick={goToGermanHandler}></button>
-        <button className='btn btn-eng' onClick={goToEnglishHandler}></button>
+      <div className="description">
+        <button className="btn btn-ger" onClick={goToGermanHandler}></button>
+        <button className="btn btn-eng" onClick={goToEnglishHandler}></button>
         <div className="experience">
-          <p className='heading heading_prof'>
+          <p className="heading heading_prof">
+            <span>
+              <img className="imgLink" src={personalInfo} alt="" />
+            </span>
             {info.motivationTitle}
           </p>
 
-          <hr className='line'></hr>
+          <hr className="line"></hr>
           <Topic description={info.motivationText} />
 
-          <div className='experience'>
-            <p className='heading heading_prof'>Professional Experience </p></div>
-          <hr className='line'></hr>
-          <Topic organization={info.BiProfOrg} title={info.BiProfTitle} description={info.BiProfDesc} />
-          <Topic organization={info.GenProfOrg} title={info.GenProfTitle} description={info.GenProfDesc} />
-          <Topic organization={info.JafiProfOrg} title={info.JafiProfTitle} description={info.JafiProfDesc} />
+          <div className="experience">
+            <p className="heading heading_prof">
+              <span>
+                <img className="imgLink" src={manager} alt="" />
+              </span>
+              Professional Experience{" "}
+            </p>
+          </div>
+          <hr className="line"></hr>
+          <Topic
+            organization={info.BiProfOrg}
+            title={info.BiProfTitle}
+            description={info.BiProfDesc}
+          />
+          <Topic
+            organization={info.GenProfOrg}
+            title={info.GenProfTitle}
+            description={info.GenProfDesc}
+          />
+          <Topic
+            organization={info.JafiProfOrg}
+            title={info.JafiProfTitle}
+            description={info.JafiProfDesc}
+          />
 
-          <div className='experience'>
-            <p className='heading heading_prof'>{info.professionalCourses}</p></div>
-          <hr className='line'></hr>
+          <div className="experience">
+            <p className="heading heading_prof">
+              <span>
+                <img className="imgLink" src={eLearning} alt="" />
+              </span>
+              {info.professionalCourses}
+            </p>
+          </div>
+          <hr className="line"></hr>
 
-          <p className='heading_prof heading_org'>{info.courses}</p>
+          <p className="heading_prof heading_org">{info.courses}</p>
           <CourseItem courseName={info.courseReact} />
           <CourseItem courseName={info.courseWeb} />
           <CourseItem courseName={info.courseJS} />
@@ -129,26 +231,35 @@ function Resume() {
           <CourseItem courseName={info.courseHtml} />
         </div>
 
-        <div className='experience'>
-          <p className='heading heading_prof'>{info.educationTitle} </p>
-          <hr className='line'></hr>
-          <Topic organization={info.universityTitle1} title={info.MaDegree} description={info.MaDesc} />
-          <Topic organization={info.universityTitle2} title={info.BaDegree} description={info.BaDesc} />
-
-
+        <div className="experience">
+          <p className="heading heading_prof">
+            <span>
+              <img className="imgLink" src={graduation} alt="" />
+            </span>
+            {info.educationTitle}{" "}
+          </p>
+          <hr className="line"></hr>
+          <Topic
+            organization={info.universityTitle1}
+            title={info.MaDegree}
+            description={info.MaDesc}
+          />
+          <Topic
+            organization={info.universityTitle2}
+            title={info.BaDegree}
+            description={info.BaDesc}
+          />
         </div>
         <footer>
-          <div className='footer_style'>
-            <div className='copy'>
-               Svirin Victoria 2023 &copy;
+          <div className="footer_style">
+            <div className="copy">Svirin Victoria 2023 &copy;</div>
+
+            <div>
+              <a href="https://uxwing.com//">Icons are taken from uxwing</a>
             </div>
-
-            <div><a href='https://uxwing.com//'> Icons are taken from uxwing</a></div>
-           </div>
-
+          </div>
         </footer>
       </div>
-
     </div>
   );
 }
